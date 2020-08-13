5a4d7a07a7b551fced1884fbc0b172b415a26080
User <User@DESKTOP-3G91FSO>
2020-08-14 02:04:32 +0800

    增加日期顯示

--- Wallpaper/src/main/js/clock.js
+++ Wallpaper/src/main/js/clock.js
@@ -1,25 +1,47 @@
 function elecClock() {
 	var digElemArr = document.querySelectorAll('div.num');
-	var dayElemArr = document.querySelectorAll('div.days > span');
+	var yearElemArr = document.querySelectorAll('div.yyyy');
+	var monElemArr = document.querySelectorAll('div.mm');
+	var dateElemArr = document.querySelectorAll('div.dd');
+	
 	function setTime () {
 		var currentTimeArr = /\s(\d)(\d)\:(\d)(\d)\:(\d)(\d)\s/.exec(new Date().toString());
 		for (var i = 1; i < currentTimeArr.length; i ++) {
-			digElemArr[i-1].setAttribute('class', 'num digit' + currentTimeArr[i])
+			digElemArr[i-1].setAttribute('class', 'num digit' + currentTimeArr[i]);
 		}
 	}
-	function setWeek () {
+	function setCalendar () {
 		var currentDay = /^.{3}/.exec(new Date().toString())[0];
-		for (var i = 0; i < dayElemArr.length; i ++) {
-			var current = dayElemArr[i];
-			if(current.innerHTML === currentDay) {
-				current.classList.add('currentDay');
-			} else {
-				current.classList.remove('currentDay');
-			}
+		var year = (new Date().getFullYear());
+		var mon = ((new Date().getMonth()) + 1 < 10) ? "0" + ((new Date().getMonth()) + 1) : (new Date().getMonth()) + 1;
+		var date = (new Date().getDate());
+		
+		var yearArr = year.toString(10).replace(/\D/g, '0').split('').map(Number);
+		var monArr = mon.toString(10).replace(/\D/g, '0').split('').map(Number);
+		var dateArr = date.toString(10).replace(/\D/g, '0').split('').map(Number);
+		for (var i = 0; i < yearArr.length; i ++) {
+			yearElemArr[i].setAttribute('class', 'yyyy day-digit' + yearArr[i]);
+		}
+		for (var i = 0; i < monArr.length; i ++) {
+			monElemArr[i].setAttribute('class', 'mm day-digit' + monArr[i]);
+		}
+		for (var i = 0; i < dateArr.length; i ++) {
+			dateElemArr[i].setAttribute('class', 'dd day-digit' + dateArr[i]);
 		}
 	}
+	
 	setTime();
-	setWeek();
+	setCalendar();
 	setInterval(setTime, 1000);
-	setInterval(setWeek, 1000 * 60);
+	setInterval(setCalendar, 1000 * 60);
+	
+	//$.getJSON("main/img/picUrl.json", function( data ) {
+	//	$.each( data, function( key, val ) {
+	//		if ('Lamy' == key) {
+	//			$('#main').css("background-image", 'url(' + val[0].localUrl + ')');
+	//			$('.clock').css("top", val[0].top);
+	//			$('.clock').css("left", val[0].left);
+	//		}
+	//	});
+	//});
 }
