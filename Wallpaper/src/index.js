e97e35906a2bb879137407cbbd6a43dee5844fef
User <User@DESKTOP-3G91FSO>
2020-08-27 00:03:00 +0800

    增加音頻顯示

--- Wallpaper/src/index.js
+++ Wallpaper/src/index.js
@@ -16,6 +16,7 @@ var renderer,
     primaryColor = null,
     secondaryColor = null,
     thirdColor = null,
+    audioColor = null,
     bg_file,
     move_bg,
     bass_wireframe;
@@ -43,6 +44,11 @@ window.wallpaperPropertyListener = {
             thirdColor = getRgb(properties.third_color)
         }
 
+        // 預設音頻顏色
+        if (audioColor === null) {
+        	audioColor = getRgb(properties.audio_color);
+        }
+
         if (properties.primary_color) {
             primaryColor = getRgb(properties.primary_color)
             setBackground()
@@ -58,15 +64,19 @@ window.wallpaperPropertyListener = {
             setBackground()
         }
 
+        // 背景設置
         if (properties.custom_image) {
             bg_file = properties.custom_image.value
             setBackground()
         }
         
+        // 是否顯示雪花
         if (properties.show_snow) {
         	var isShow = properties.show_snow.value;
         	document.getElementById('snow-container').style.display = (isShow) ? "inline-block" : "none";
         }
+        
+        // 是否顯示日期
         if (properties.show_calendar) {
         	var isShowCalendar = properties.show_calendar.value;
         	if (isShowCalendar) {
@@ -76,18 +86,22 @@ window.wallpaperPropertyListener = {
         	}
         }
 
+        // 日期X軸位置
         if (properties.calendar_x) {
         	document.getElementById('calendar').style.left = properties.calendar_x.value + "%";
         }
 
+        // 日期Y軸位置
         if (properties.calendar_y) {
         	document.getElementById('calendar').style.top = properties.calendar_y.value + "%";
         }
 
+        // 日期顏色
         if (properties.calendar_color) {
         	setCalendarColor(getRgb(properties.calendar_color));
         }
 
+        // 是否顯示時鐘
         if (properties.show_clock) {
         	var isShow = properties.show_clock.value;
         	if (isShow) {
@@ -97,26 +111,85 @@ window.wallpaperPropertyListener = {
         	}
         }
 
+        // 時鐘X軸位置
         if (properties.clock_x) {
         	$('.clock').css('left', properties.clock_x.value + "%");
         }
 
-
+        // 時鐘Y軸位置
         if (properties.clock_y) {
         	$('.clock').css('top', properties.clock_y.value + "%");
         }
 
+        // 時鐘顏色
         if (properties.clock_color) {
         	setClockColor(getRgb(properties.clock_color));
+        }
+        
+
+        // 音頻顏色
+        if (properties.audio_color) {
+        	audioColor = getRgb(properties.audio_color)
+            setAudioColor()
         }
     }
 }
 
-window.onload = function () {
-    init();
-    window.wallpaperRegisterAudioListener(wallpaperAudioListener);
+function wallpaperAudioListener(audioArray) {
+	// 音頻陣列為128
+	var arr = new Array(128);
+	
+    // 將左右聲道合併 
+    for (var i = arr.length/2 - 1; i >= 0; i--) {
+        arr[i] = (Math.floor(audioArray[i]*1000) + Math.floor(audioArray[arr.length/2+i]*1000))/2;
+    }
+    audios(audioArray);
 }
 
+function audios(array){
+	// 取得canvas標籤
+	var canvas = document.getElementById("audios");
+	// 調整canvas屬性
+	var ctx = canvas.getContext("2d");
+	
+	// 每次寫入音頻時，清除前次的音頻
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.beginPath();
+	
+	// 產生位置左方間隙
+	var jx_left = 0;
+	
+	// 每個音條寬度。預設寬度為使用者螢幕寬度/64
+	var jx_width = window.innerWidth/64;
+	
+	// 每個音條的間隙
+	var spacing = 1;
+	
+	// 取得間隙後開始繪製
+	for (var i = 0; i <= array.length/2-1; i++) {
+		// 高度
+		var jx_height = 0;
+		// 產生音頻
+		ctx.fillRect(jx_left, jx_height, jx_width, array[i]*500);
+		// 取得下一個音條的位置
+		jx_left += jx_width+spacing;
+    }
+    // 音條顏色
+    ctx.fillStyle = audioColor;
+    // 畫上去
+    ctx.fill();
+    // 結束
+    ctx.closePath();
+}
+
+// 設定音條顏色
+function setAudioColor() {
+	var canvas = document.getElementById("audios");
+	var ctx = canvas.getContext("2d");
+    ctx.fillStyle = audioColor;
+}
+
+// 取得RGB色碼
 function rgb2hex(rgb) {
     rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
     return (rgb && rgb.length === 4) ? "" +
@@ -125,6 +198,7 @@ function rgb2hex(rgb) {
     ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
 }
 
+// 設定時鐘顏色
 function setClockColor(clockColor) {
 	for (var j = 1; j <= 7; j++) {
 		var classList = document.getElementsByClassName('cell' + j);
@@ -141,6 +215,7 @@ function setClockColor(clockColor) {
 	}
 }
 
+// 設定日期顏色
 function setCalendarColor(calendarColor) {
     for (var j = 1; j <= 7; j++) {
         var classList = document.getElementsByClassName('yearCell' + j);
@@ -179,6 +254,7 @@ function setCalendarColor(calendarColor) {
 	}
 }
 
+// 設定背景圖片/顏色
 function setBackground() {
     var pC = primaryColor,
         sC = secondaryColor,
@@ -201,10 +277,6 @@ function setBackground() {
     }
 }
 
-function wallpaperAudioListener(audioArr) {
-    audioArray = audioArr;
-}
-
 function debug(write) {
     if (write === undefined) {
         write = 'undefined'
@@ -225,3 +297,7 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+window.onload = function () {
+    init();
+    window.wallpaperRegisterAudioListener(wallpaperAudioListener);
+}
